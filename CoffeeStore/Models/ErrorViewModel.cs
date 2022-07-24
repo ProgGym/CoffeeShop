38c264bea6e7da069eb457a06a2c77aeb5517cd0
Андрей Губин <andrei28122000@gmail.com>
2022-07-24 21:38:32 +0500

    Добавьте файлы проекта.

---
+++ CoffeeStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgGym.CoffeeShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
