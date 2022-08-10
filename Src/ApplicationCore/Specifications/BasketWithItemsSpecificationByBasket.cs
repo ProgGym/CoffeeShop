e1f8edd253a0bdd6a20c2b5d7d8f65ebaf9b854d
NotTastyCupcake <andrei28122000@gmail.com>
2022-08-10 20:41:38 +0500

    feat: Добавил поиск корзины по Id

---
+++ Src/ApplicationCore/Specifications/BasketWithItemsSpecificationByBasket.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Specification;
+using ProgGym.CoffeeShop.ApplicationCore.Entities.BasketAggregate;
+
+namespace ProgGym.CoffeeShop.ApplicationCore.Specifications;
+
+public sealed class BasketWithItemsSpecificationByBasket : Specification<Basket>, ISingleResultSpecification
+{
+    public BasketWithItemsSpecificationByBasket(Guid basketId) 
+    {
+        Query
+            .Where(b => b.Id == basketId)
+            .Include(b => b.Items);
+    }
+}
---
+++ Src/ApplicationCore/Specifications/BasketWithItemsSpecificationByUser.cs
@@ -0,0 +1,20 @@
+﻿using Ardalis.Specification;
+using ProgGym.CoffeeShop.ApplicationCore.Entities.BasketAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgGym.CoffeeShop.ApplicationCore.Specifications;
+
+public sealed class BasketWithItemsSpecificationByUser : Specification<Basket>, ISingleResultSpecification
+{
+    public BasketWithItemsSpecificationByUser(Guid buyerId)
+    {
+
+        Query
+            .Where(b => b.UserId == buyerId)
+            .Include(b => b.Items);
+    }
+}
