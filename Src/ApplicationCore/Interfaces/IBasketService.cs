7bf2c670549cf1bdd8bf229dade4142e659b2a80
NotTastyCupcake <andrei28122000@gmail.com>
2022-08-10 20:40:18 +0500

    feat: Добавил интерфейс для Service

---
+++ Src/ApplicationCore/Interfaces/IBasketService.cs
@@ -0,0 +1,10 @@
+﻿using ProgGym.CoffeeShop.ApplicationCore.Entities.BasketAggregate;
+
+namespace ProgGym.CoffeeShop.ApplicationCore.Interfaces;
+
+public interface IBasketService
+{
+    Task<Basket> AddItemAsync(Guid userId, int catalogItemId, decimal price, int quentity = 1);
+    Task<Basket> DeleteItemAsync(int catalogItemId);
+    Task DeleteBasketAsync(Guid basketId);
+}
