708f5fcc8837ef12887a7d7c8e832bf0f1edb735
NotTastyCupcake <andrei28122000@gmail.com>
2022-08-10 20:42:10 +0500

    test: Добавил юнит тест для поиска корзины

---
+++ Tests/UnitTests/ApplicationCore/Specifications/BasketWhithItemsSpecificationByUserTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ProgGym.CoffeeShop.UnitTests.ApplicationCore.Specifications;
+
+public class BasketWhithItemsSpecificationByUserTest
+{
+    [Test]
+    public void GetBasketWhithItemByUserId()
+    {
+        // Orange
+
+        // Act
+
+        // Assert
+
+    }
+}
--- Tests/UnitTests/UnitTest1.cs
+++ Tests/UnitTests/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace ProgGym.CoffeeShop.UnitTests
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
